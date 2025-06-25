b46da93273a2a586a618c90d8a7041a5c2915691
anshbhardwaj08 <anshrbhardwaj1@gmail.com>
2025-06-25 15:40:12 +0530

    Time: 10 ms (23.77%), Space: 17.8 MB (87.32%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.cpp
@@ -4,25 +4,48 @@ public:
         int n=nums.size();
         int m=queries.size();
         sort(nums.begin(),nums.end());
-        int i=1;
-        while(i<n){
-            nums[i]=nums[i]+nums[i-1];
-            i++;
+        int k=1;
+        while(k<n){
+            nums[k]=nums[k]+nums[k-1];
+            k++;
         }
         vector<int>answer;
 
-        for(int i=0;i<m;i++){
-            int len=0;
-            for(int j=0;j<n;j++){
-                if(nums[j]>queries[i]) break;
-                else{
-                    len++;
-                }
-            }
-            answer.push_back(len);
-        }
+        // for(int i=0;i<m;i++){
+        //     int len=0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[j]>queries[i]) break;
+        //         else{
+        //             len++;
+        //         }
+        //     }
+        //     answer.push_back(len);
+        // }
  
+        int i=0;
         
+        
+        while(i<m){
+            int size=0;
+            int j=0;
+            while(j<n){
+                if(nums[j]>queries[i]){
+                    
+                    break;
+
+                } 
+                else{
+                    size++;
+                    
+                }
+                j++;
+                
+                
+            }
+            answer.push_back(size);
+            
+            i++;
+        }
 
         return answer;
     }
