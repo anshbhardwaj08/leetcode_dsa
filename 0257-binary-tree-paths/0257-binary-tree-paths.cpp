5a1094dad3bdca94900b992209a2581ee6506ee8
anshbhardwaj08 <anshrbhardwaj1@gmail.com>
2025-07-14 19:49:48 +0530

    Time: 3 ms (29.61%), Space: 17.8 MB (36.99%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -5,7 +5,8 @@ public:
     void helper(TreeNode* root,string s, vector<string>&ans){
         if(root==NULL) return ;
         string a=to_string(root->val);
-        if(root->left==NULL && root->right==NULL){
+        
+        if(root->left==NULL && root->right==NULL){ // to know whether it is a leaf node or not;
             s=s+a;
             ans.push_back(s);
             return ;
