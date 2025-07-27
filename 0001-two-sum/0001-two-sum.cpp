2f02830881f1fdee6ef09c2494d0a22a1928a3df
anshbhardwaj08 <anshrbhardwaj1@gmail.com>
2025-07-27 23:07:16 +0530

    Time: 8 ms (44.3%), Space: 15.9 MB (8.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>res;
-        bool flag =false;
-        int n=nums.size();
-        for(int i=0; i<n; i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    flag=true;
-                    res.push_back(i);
-                    res.push_back(j);                 
-                    break;
-                }
+    //     vector<int>res;
+    //     bool flag =false;
+    //     int n=nums.size();
+    //     for(int i=0; i<n; i++){
+    //         for(int j=i+1;j<n;j++){
+    //             if(nums[i]+nums[j]==target){
+    //                 flag=true;
+    //                 res.push_back(i);
+    //                 res.push_back(j);                 
+    //                 break;
+    //             }
 
 
-            }
+    //         }
+    //     }
+    //     if(flag==false){
+    //         res.push_back(-1);
+    //         res.push_back(-1);
+    //         return res;
+    //     };
+    //     return res;
+    // }
+
+    unordered_map<int,int>m;
+    int n=nums.size();
+    int i=0;
+    while(i<n){
+        m[nums[i]]=i;
+        i++;
+    }
+    vector<int>v;
+    for(int i=0;i<n;i++){
+        int y=target-nums[i];
+        if(m.find(y)!=m.end() && m[y]!=i){
+            v.push_back(i);
+            v.push_back(m[y]);
+            break;
         }
-        if(flag==false){
-            res.push_back(-1);
-            res.push_back(-1);
-            return res;
-        };
-        return res;
+    }
+    return v;
     }
 };
