aafe32f36e6517947118c4e3eccbaa39e8b51350
anshbhardwaj08 <anshrbhardwaj1@gmail.com>
2025-08-14 16:10:40 +0530

    Time: 1 ms (18.52%), Space: 9.7 MB (64.46%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int n) {
+    // long double fact(long double a){
+    //     int i=2; 
+    //     long double b=1;
+    //     if(a==0 || a==1) return 1;
+    //     while(i<=a){
+    //         b=b*i;
+    //         i++;
+    //     } 
 
-        vector<vector<int>>ans;
-        for(int i=0;i<n; i++){
-            vector<int>a(i+1);
-            ans.push_back(a);
+    //     return b;
 
+    // }
+    // int nCr(int h, int x){
+    //     long double c=fact(h);
+    //     long double m=(fact(h-x)*fact(x));
+
+    //     return static_cast<int>(c / m);
+    // }
+    int nCr(int n, int r) {
+        long long res = 1;
+        for (int i = 1; i <= r; i++) {
+            res = res * (n - i + 1) / i;
         }
-        
-        
-        for(int i=0;i<n;i++){
+        return static_cast<int>(res);
+    }
+    vector<vector<int>> generate(int k) {
+        vector<vector<int>> ans;
+        if(k==1){
+            vector<int>q;
+            q.push_back(1);
+            ans.push_back(q);
+            return ans;
+        }
+        for(int i=0; i<k ;i++){
+            vector<int>v;
             for(int j=0;j<=i;j++){
-                if(j==0 || j==i){
-                    ans[i][j]=1;
-                }
-                else{
-                    ans[i][j]=ans[i-1][j] + ans[i-1][j-1];
-                }
+                
+                v.push_back(nCr(i,j));
             }
-
+            ans.push_back(v);
         }
         return ans;
-
-
+        
     }
 };
