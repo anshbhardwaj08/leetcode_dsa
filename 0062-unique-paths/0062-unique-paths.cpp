94097f970abd84cbca11edbaf72cac5580fde5f1
anshbhardwaj08 <anshrbhardwaj1@gmail.com>
2026-02-17 14:00:45 +0530

    Time: 0 ms (100%), Space: 9.4 MB (52.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& grid,int i,int j,vector<vector<int>>& dp){
-        int m=grid.size();
-        int n=grid[0].size();
-        if(i>=m || j>=n){
-            return 0;
-        }
-        if(i==m-1 && j==n-1){
-            return 1;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        return dp[i][j]=helper(grid,i,j+1,dp)+helper(grid,i+1,j,dp);
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>grid(m,vector<int>(n,1));
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return helper(grid,0,0,dp);
-
+        // tabulation
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int i=0;i<dp.size();i++){
+            for(int j=0;j<dp[0].size();j++){
+                if(i==0 || j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
