7efe88b9ec15dd0fa09ff4f45a5651dce8c77919
anshbhardwaj08 <anshrbhardwaj1@gmail.com>
2026-04-09 13:51:55 +0530

    Time: 17 ms (19.61%), Space: 18.3 MB (28.02%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<vector<int>>> dp;
+
+    int f(int ind,int buy,int cap,vector<int>& prices){
+        if(ind==prices.size()|| cap==0){
+            return 0;
+        }
+        if(dp[ind][buy][cap]!=-1){
+            return dp[ind][buy][cap];
+        }
+        if(buy==1){
+            return dp[ind][buy][cap]= max(-prices[ind]+f(ind+1,0,cap,prices),f(ind+1,1,cap,prices));
+        }
+        
+            return dp[ind][buy][cap]= max(prices[ind]+f(ind+1,1,cap-1,prices),f(ind+1,0,cap,prices));
+        
+        
+
+        
+
+    }
+    int maxProfit(int k ,vector<int>& prices) {
+        int n=prices.size();
+        dp.resize(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        int ans=f(0,1,k,prices);
+        if(ans>0){
+            return ans;
+        }
+        else{
+            return 0;
+        }
+    }
+};
