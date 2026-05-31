ca8bbf6e686fa692dc976d78fc05f48067b0bf29
anshbhardwaj08 <anshrbhardwaj1@gmail.com>
2026-05-31 19:11:49 +0530

    Time: 3 ms (9.43%), Space: 9.6 MB (28.66%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int solve(int i,int j){
-        if(i==0 && j==0){
-            return 1;
-        }
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int up=solve(i-1,j);
-        int left=solve(i,j-1);
-        return dp[i][j]=up+left;
+    // int solve(int i,int j){
+    //     if(i==0 && j==0){
+    //         return 1;
+    //     }
+    //     if(i<0 || j<0){
+    //         return 0;
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     int up=solve(i-1,j);
+    //     int left=solve(i,j-1);
+    //     return dp[i][j]=up+left;
 
 
-    }
+    // }
     int uniquePaths(int m, int n) {
         dp.clear();
         dp.resize(m,vector<int>(n,-1));
-        int ans=solve(m-1,n-1);
-        return ans;
+        // int ans=solve(m-1,n-1);
+        if(m==1 && n==1){
+            return 1;
+        }
+        // return ans;
+        dp[0][0]=0;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return abs(dp[m-1][n-1]);
     }
 };
