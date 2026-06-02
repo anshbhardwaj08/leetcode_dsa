95767a09f5e258561a3fe4a13701c34429984204
anshbhardwaj08 <anshrbhardwaj1@gmail.com>
2026-06-02 12:41:23 +0530

    Time: 142 ms (39.71%), Space: 20.7 MB (51.29%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    bool solve(vector<int>& nums, int i , int target){
-        if(i==0){
-            return (nums[i]==target);
-        }
-        if(target==0){
-            return true;
-        }
-        if(dp[i][target]!=-1){
-            return dp[i][target];
-        }
-        bool not_take=solve(nums,i-1,target);
-        bool take =false;
-        if(target>=nums[i]){
-            take=solve(nums,i-1,target-nums[i]);
-        }
-        return dp[i][target]=take || not_take;
-    }
+    vector<vector<bool>>dp;
+    // bool solve(vector<int>& nums, int i , int target){
+    //     if(i==0){
+    //         return (nums[i]==target);
+    //     }
+    //     if(target==0){
+    //         return true;
+    //     }
+    //     if(dp[i][target]!=-1){
+    //         return dp[i][target];
+    //     }
+    //     bool not_take=solve(nums,i-1,target);
+    //     bool take =false;
+    //     if(target>=nums[i]){
+    //         take=solve(nums,i-1,target-nums[i]);
+    //     }
+    //     return dp[i][target]=take || not_take;
+    // }
     bool canPartition(vector<int>& nums) {
         int target=0;
         for(int i=0;i<nums.size();i++){
@@ -27,9 +27,24 @@ public:
             return false;
         }
         dp.clear();
-        dp.resize(nums.size(),vector<int>(target+1,-1));
-        bool ans=solve(nums,nums.size()-1,target/2);
-        return ans;
+        dp.resize(nums.size(),vector<bool>(target+1,false));
+        // bool ans=solve(nums,nums.size()-1,target/2);
+        // return ans;
+        for(int i=0;i<nums.size();i++){
+            dp[i][0]=true;
+        }
+        dp[0][nums[0]]=true;
+        for(int i=1;i<nums.size();i++){
+            for(int k=1;k<=target/2;k++){
+                bool not_take=dp[i-1][k];
+                bool take =false;
+                if(k>=nums[i]){
+                    take=dp[i-1][k-nums[i]];
+                }
+                dp[i][k]=take | not_take;
+            }
+        }
+        return dp[nums.size()-1][target/2];
 
     }
 };
