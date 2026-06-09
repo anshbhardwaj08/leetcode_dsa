3f1a85e51f97926acc5c4f02c86a25cdf3d2c820
anshbhardwaj08 <anshrbhardwaj1@gmail.com>
2026-06-09 23:41:13 +0530

    Time: 8 ms (46.96%), Space: 13.4 MB (28.49%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -22,8 +22,27 @@ public:
         int n=s.size();
         int m=t.size();
         dp.clear();
-        dp.resize(n,vector<int>(m,-1));
-        int ans=solve(n-1,m-1,s,t);
-        return ans;
+        dp.resize(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    int take=dp[i-1][j-1];
+                    dp[i][j]=take;
+                }
+                else{
+                    dp[i][j]=1+min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
+                }
+                
+            }
+        }
+        return dp[n][m];
+        // int ans=solve(n-1,m-1,s,t);
+        // return ans;
     }
 };
